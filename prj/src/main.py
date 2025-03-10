3136e12b263245260bda43a3fb3ed8113a91cd9b
ZVS <graunter@gmail.com>
2025-03-10 13:02:16 +0300

    an avaliable port list detection

--- prj/src/main.py
+++ prj/src/main.py
@@ -1,7 +1,57 @@
-import TermTk as ttk
+import TermTk as ttk        # pip3 install --upgrade pyTermTk
+from TermTk import TTkLog, TTkTerm
 import sys, os, argparse
+import sys
+import glob
+import serial               #pip install pyserial
+
+port_lst_names = None
+
+def serial_ports():
+    """ Lists serial port names
+
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+
+    return result
+
+def on_scan_btn():
+    global port_lst_names
+
+    # TODO: reselect the curent port name if present in rescan
+    result = serial_ports()
+
+    while port_lst_names.items():
+        port_lst_names.removeAt(0)
+
+    port_lst_names.addItems(result)
+
 
 def BuildMainScreen(root=None):
+
+    global port_lst_names
 
     root_layout = ttk.TTkVBoxLayout()
     root.setLayout(root_layout)
@@ -15,7 +65,7 @@ def BuildMainScreen(root=None):
     port_lst_layout = ttk.TTkVBoxLayout()
     port_lst_frame.setLayout(port_lst_layout)
 
-    port_lst_names = ttk.TTkList(name="Port list", border=True, items=["Item 1","Item 2","Item 3"])
+    port_lst_names = ttk.TTkList(name="Port list", border=True, items=serial_ports())
     port_lst_layout.addWidget(port_lst_names)
 
     port_arg_frame = ttk.TTkFrame(border=True, title="Call arguments")
@@ -34,7 +84,10 @@ def BuildMainScreen(root=None):
 
 
     btns_line_layout.addWidget(ttk.TTkButton(text = "Open", border=True))
-    btns_line_layout.addWidget(ttk.TTkButton(text = "Scan", border=True))
+
+    scan_btn = ttk.TTkButton(text = "Scan", border=True)
+    scan_btn.clicked.connect(on_scan_btn)
+    btns_line_layout.addWidget(scan_btn)
 
     small_btns_frame = ttk.TTkFrame(border=0)
     small_btns_layout = ttk.TTkHBoxLayout()
@@ -48,8 +101,11 @@ def BuildMainScreen(root=None):
 
     btns_line_layout.addWidget(small_btns_frame)
 
-    btns_line_layout.addWidget(ttk.TTkButton(text = "Close", border=True))     
-    btns_line_layout.addWidget(ttk.TTkButton(text = "Exit", border=True))     
+    btns_line_layout.addWidget(ttk.TTkButton(text = "Close", border=True))   
+
+    exit_btn = ttk.TTkButton(text = "Exit", border=True)
+    exit_btn.clicked.connect(ttk.TTkHelper.quit)
+    btns_line_layout.addWidget(exit_btn)     
 
 def main():
     parser = argparse.ArgumentParser()
